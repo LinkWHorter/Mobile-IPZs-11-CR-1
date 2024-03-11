39cf421ab4212e1419e3518a13e653306c92adcb
LinkWHorter <LinkWHorter@gmail.com>
2024-03-11 14:18:07 +0200

    Console Work Tests

---
+++ app/src/main/java/ipz/kovalov/mobile1/MainActivity.kt
@@ -0,0 +1,8 @@
+package ipz.kovalov.mobile1
+
+fun main() {
+    val firstNumber = 10
+    val secondNumber = 5
+    val result = firstNumber + secondNumber
+    println("$firstNumber + $secondNumber = $result")
+}
